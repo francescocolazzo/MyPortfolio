38b3fa731e412234de1d945fe9bc7e3badab10f2
Francesco Colazzo <francesco.colazzo@dontouch.ch>
2023-06-04 00:17:40 +0200

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,8 +2,8 @@
     <div class="text-center">
         <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hi! It's Francesco Colazzo</h2>
         <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">   
-            Passionate and proactive, I apply myself in everything I do.
-            I like to make modern web applications through several technologies that allows me to improve my skills and my knowledge.
+            Passionate and proactive, I apply myself in everything I do. 
+            I like developing modern web applications by several technologies that allows me to improve my skills and knowledge.
         </p>
         <p class="text-center text-green-600">#Frontend #Backend #WebApplication #PHP</p>
     </div>
