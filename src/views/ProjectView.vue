0e4110dbfbf28893058de16bd0415aa64e2f7255
Francesco Colazzo <francesco.colazzo@dontouch.ch>
2024-01-12 10:09:30 +0100

    update

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -25,7 +25,7 @@ const error = ref(null);
 onMounted(async () => {
     try {
         loading.value = true;
-        const res = await fetch('https://api.github.com/users/jack19911/repos');
+        const res = await fetch('https://api.github.com/users/francescocolazzo/repos');
         projects.value = await res.json();
         loading.value = false;
     } catch (ex) {
